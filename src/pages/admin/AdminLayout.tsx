6a7a9db9d52ae3a692c92ec2b8cd02205201d646
devfhero <50680346+simple-easydev@users.noreply.github.com>
2026-02-12 10:28:09 +0800

    fixed the style of content's padding

--- src/pages/admin/AdminLayout.tsx
+++ src/pages/admin/AdminLayout.tsx
@@ -9,7 +9,7 @@ export default function AdminLayout() {
       <AppSidebar />
       <SidebarInset>
         <TopBar />
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+        <div className="flex flex-1 flex-col gap-4 p-2">
           <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min p-4 md:p-6 lg:p-8">
             <Outlet />
           </div>
